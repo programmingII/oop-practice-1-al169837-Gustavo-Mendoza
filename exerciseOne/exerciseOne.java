da6f62e432fbbe29ffe47cb3b57321731ea39474
Gustavo Mendoza <43121456+al169837-Gustavo-Mendoza@users.noreply.github.com>
2019-02-12 13:46:35 -0700

    Update exerciseOne.java

--- exerciseOne/exerciseOne.java
+++ exerciseOne/exerciseOne.java
@@ -2,8 +2,8 @@
 //02/07/2019
 //12:29
 
-class exerciseOne{
-	public static void main(String[] args){
+class exerciseOne{ //contenido general de lo que se quiere hacer
+	public static void main(String[] args){ //public : sera publico el programa
   		System.out.println("Hello! \nGustavo Mendoza");
    	}
 }
