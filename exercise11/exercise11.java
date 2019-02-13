e45bbfcff6ec2f39d7ef1076a9dc64159bc29899
Gustavo Mendoza <43121456+al169837-Gustavo-Mendoza@users.noreply.github.com>
2019-02-13 10:48:33 -0700

    Add files via upload

---
+++ exercise11/exercise11.java
@@ -0,0 +1,13 @@
+//Gustavo Alejandro Mendoza Cerna       Día 13/02/19    Hora: 10:33
+
+class exercise11{
+	public static void main(String[] args){
+		double radio = 7.5;
+		double area = Math.PI * (radio*radio);
+		double perimetro = Math.PI * (2*radio);		
+
+		System.out.println("El area es: "+ area);
+		System.out.println("El perimetro es: "+ perimetro);
+		
+	}	
+}
